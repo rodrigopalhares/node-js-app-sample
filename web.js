3acc1656b46910a40029226c80ba60ab65a73ba4
rodrigopalhares <palhares.rodrigo@gmail.com>
2013-03-06 18:45:36 -0300

    ejs integration

--- web.js
+++ web.js
@@ -2,8 +2,22 @@ var express = require('express');
 
 var app = express();
 
+// https://github.com/visionmedia/express/blob/master/examples/ejs/index.js
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	response.send('Hello World!');
+});
+
+app.get('/linkedin', function(req, res) {
+
+	res.render('linkedin', {
+		code: req.query.code,
+		state: req.query.state
+	});
+
 });
 
 var port = process.env.PORT || 5000;
